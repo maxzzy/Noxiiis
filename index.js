7b4d462059fecf90fe9e9b62894d546123ff3778
maxzzy <73212216+maxzzy@users.noreply.github.com>
2021-06-20 01:03:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const axios = require("axios")
-const urls = ["https://fvckboy.glitch.me", "https://bot-paw-arka.glitch.me"]
+const urls = ["https://fvckboy.glitch.me"]
+setInterval(function() {
+            urls.forEach(url => {
+            axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
+        })
+    }, 60 * 1000);
+
+const urls = ["https://bot-paw-arka.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
